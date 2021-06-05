47c34c6cfa33527bfd7eb62e530dc797cc34daee
Christopher Smith <christopher.d.smith4@gmail.com>
2021-06-05 12:22:20 -0700

    Added rss feed

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from blogging.views import BlogListView, BlogDetailView
+from blogging.feeds import LatestEntriesFeed
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("rss", LatestEntriesFeed()),
 ]
